5b2b3bdd5d3218ec54ad598376b195ddd708216d
xavian1996 <othman.elmzalni@gmail.com>
2020-04-19 20:31:25 +0000

    Add files via upload
    SCRIPT CREATED USING PYTHON AND PYTUBE LIB

---
+++ youtube-downloader.py
@@ -0,0 +1,59 @@
+from pytube import YouTube
+from pytube import Playlist
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def Download():
+
+
+    form = input("SELECT A FORMAT :\n1. FOR VIDEO\n2. FOR ONLY AUDIO\n3. FOR PLAYLIST\n"
+                 "YOUR CHOICE : ")
+    vid_url = input("URL => ")
+    yt = YouTube(vid_url)
+    stream = yt.streams.first()
+
+    if form == "1":
+        try:
+            print(f"{bcolors.WARNING}================================================")
+            print(f"{bcolors.WARNING}=  Script Created By SELF LEARNING (XxavianxX) =")
+            print("================================================")
+            print(f"{bcolors.WARNING}\n[+] DOWNLOADING THE VIDEO PLEASE WAIT ...")
+            stream = yt.streams.first()
+            stream.download()
+            print(f"{bcolors.OKGREEN}[*] DOWNLOAD FINISHED !")
+        except:
+            print(f"{bcolors.FAIL}[!] ERROR ! TRY AGAIN")
+            return
+    elif form == "2":
+        try :
+            print(f"{bcolors.WARNING}================================================")
+            print(f"{bcolors.WARNING}=  Script Created By SELF LEARNING (XxavianxX) =")
+            print(f"{bcolors.WARNING}================================================")
+            print(f"{bcolors.WARNING}\n[+] DOWNLOADING THE VIDEO PLEASE WAIT ...")
+            stream = yt.streams.filter(only_audio=True)
+            stream[0].download()
+            print(f"{bcolors.OKGREEN}[*] DOWNLOAD FINISHED !")
+        except:
+            print(f"{bcolors.FAIL}[!] ERROR ! TRY AGAIN")
+            return
+    elif form == "3":
+        try :
+            print(f"{bcolors.WARNING}================================================")
+            print(f"{bcolors.WARNING}=  Script Created By SELF LEARNING (XxavianxX) =")
+            print(f"{bcolors.WARNING}================================================")
+            print(f"{bcolors.WARNING}\n[+] DOWNLOADING THE PLAYLIST PLEASE WAIT ...")
+            pl = Playlist(vid_url)
+            pl.download_all()
+            print(f"{bcolors.OKGREEN}[*] DOWNLOAD FINISHED !")
+        except:
+            print(f"{bcolors.FAIL}[!] ERROR ! TRY AGAIN")
+            return
+Download()
+
